e0cc72fd65eeb01ac946ef37e300327043e8a510
Navaneeth Krishna <navaneethkrishna@Navaneeths-MacBook-Pro.local>
2026-07-07 10:07:50 +0530

    changed image

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -9,7 +9,7 @@ const defaultOpenGraph: Metadata['openGraph'] = {
     'Discover handcrafted Indian clothing that blends timeless craftsmanship with contemporary style. Made with premium fabrics and designed to last.',
   images: [
     {
-      url: 'https://honeylooms.in/logo.svg',
+      url: 'https://honeylooms.in/logo.jpg',
       width: 1200,
       height: 630,
       alt: 'Honeylooms',
