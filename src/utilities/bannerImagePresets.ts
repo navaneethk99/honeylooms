b7f4c04b4dba27ffb8651148cbf1686731385109
Navaneeth Krishna <navaneethkrishna@Navaneeths-MacBook-Pro.local>
2026-09-03 20:07:56 +0530

    perf(images): Increase banner image quality and size

--- src/utilities/bannerImagePresets.ts
+++ src/utilities/bannerImagePresets.ts
@@ -1,7 +1,7 @@
 export const bannerImagePresets = [
-  { dimension: 640, name: 'bannerPreview', quality: 40 },
-  { dimension: 960, name: 'bannerMedium', quality: 60 },
-  { dimension: 1920, name: 'bannerLarge', quality: 75 },
+  { dimension: 960, name: 'bannerPreview', quality: 100 },
+  { dimension: 1920, name: 'bannerMedium', quality: 100 },
+  { dimension: 3230, name: 'bannerLarge', quality: 100 },
 ] as const
 
 export type BannerImagePresetName = (typeof bannerImagePresets)[number]['name']
